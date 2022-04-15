1e5c469e2e2e12a7b32c8de7f9b89fe555595e99
Brian Thomas <bthomas1@wsu.edu>
2022-04-14 18:12:25 -0700

    Added helpful script for building the evaluations.

---
+++ build_evaluation.py
@@ -0,0 +1,18 @@
+methods = ['field_mean', 'carry_forward', 'carry_backward', 'carry_average', 'zero_fill', 'regression_sgd', 'regression_rand_forest', 'gan', 'knn']
+sites = ['sttr001', 'sttr006', 'sttr008', 'sttr009', 'sttr010']
+missing = ['10', '20', '30']
+gaps = ['10', '600', '36000']
+
+for m in methods:
+    for s in sites:
+        for p in missing:
+            for g in gaps:
+                for i in range(3):
+                    msg = 'time python -u mink.py --method={} '.format(m)
+                    msg += '--imputedata=data/{}.test.{}.{}.{} '.format(s, p, g, i)
+                    msg += '--fulldata=data/{}.test '.format(s)
+                    msg += '--ignorefile=data/{}.test.ignore '.format(s)
+                    msg += '--spacing=0.1 '
+                    msg += '--model-id={}.train '.format(s)
+                    msg += '2>&1 | tee -a log/{}.{}.{}.{}.{}.eval'.format(m, s, p, g, i)
+                    print(msg)
